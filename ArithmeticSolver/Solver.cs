5468dc897a45ac9ddcf8b352c2ee56721513d081
Kiran Gadhave <kiran.gadhave@outlook.com>
2017-02-28 09:46:29 +0530

    Long expressions take time/stuck in loop. Try new approach.

--- ArithmeticSolver/Solver.cs
+++ ArithmeticSolver/Solver.cs
@@ -24,7 +24,7 @@ namespace ArithmeticSolver {
 
             var expression = ExpressionHandler.Build(tokens.ToList());
             var answer = expression.Solve(variableDictionary);
-            return 0;
+            return answer;
         }
     }
 }
--- ArithmeticSolverTestProject/SolverTest.cs
+++ ArithmeticSolverTestProject/SolverTest.cs
@@ -5,9 +5,77 @@ using ArithmeticSolver;
 namespace ArithmeticSolverTestProject {
     [TestClass]
     public class SolverTest {
+        //[TestMethod]
+        //public void SimpleAdditionTest () {
+        //    var expected = Solver.Solve("2+3");
+        //    Assert.AreEqual(expected, 5, 0.001, "Addition Failed");
+        //}
+
+        //[TestMethod]
+        //public void SimpleSubtractionTest () {
+        //    var expected = Solver.Solve("2-3");
+        //    Assert.AreEqual(expected, -1, 0.001, "Subtraction Failed");
+        //}
+
+        //[TestMethod]
+        //public void SimpleMultiplicationTest () {
+        //    var expected = Solver.Solve("2*3");
+        //    Assert.AreEqual(expected, 6, 0.001, "Multiplication Failed");
+        //}
+
+        //[TestMethod]
+        //public void SimpleDivisionTest () {
+        //    var expected = Solver.Solve("6/3");
+        //    Assert.AreEqual(expected, 2, 0.001, "Division Failed");
+        //}
+
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void DivisionByZeroTest () {
+        //    var expected = Solver.Solve("2/0");
+        //}
+
+        //[TestMethod]
+        //public void BodmasTest1 () {
+        //    var expected = Solver.Solve("2+6/3");
+        //    Assert.AreEqual(expected, 4, 0.001, "Division Failed");
+        //}
+
+        //[TestMethod]
+        //public void BodmasTest2 () {
+        //    var expected = Solver.Solve("2-6/3");
+        //    Assert.AreEqual(expected, 0, 0.001, "Division Failed");
+        //}
+
+        //[TestMethod]
+        //public void BodmasTest3 () {
+        //    var expected = Solver.Solve("4.5+6*2");
+        //    Assert.AreEqual(expected, 16.5, 0.001, "Division Failed");
+        //}
+
+        //[TestMethod]
+        //public void BodmasTest4 () {
+        //    var expected = Solver.Solve("14.5-6*2");
+        //    Assert.AreEqual(expected, 2.5, 0.001, "Division Failed");
+        //}
+
+        //[TestMethod]
+        //public void ParanthesisTest1 () {
+        //    var expected = Solver.Solve("(4.5+6)*2");
+        //    Assert.AreEqual(expected, 21, 0.001, "Division Failed");
+        //}
+
+        //[TestMethod]
+        //public void ParanthesisTest2 () {
+        //    var expected = Solver.Solve("(4.5+6)*(2+3.3) - 23");
+        //    Assert.AreEqual(expected, 32.65, 0.001, "Division Failed");
+        //}
+
         [TestMethod]
-        public void TestSolver () {
-            Solver.Solve("2+(3-2)/(5-2)");
+        public void ParanthesisTest3 () {
+            var expected = Solver.Solve(@"(2+1)/(3.3+1)");
+            Assert.AreEqual(expected, 9.893939, 0.001, "Division Failed");
         }
+
     }
 }
